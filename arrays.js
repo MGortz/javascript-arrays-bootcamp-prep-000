b351573c4fe8b538ee1c0eb3b5048d6b0a06aa95
Mike Gorton <mike.gorton1@gmail.com>
2019-01-16 19:18:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(a, b) {
   return a
 }
 
-function destructivelyAddElementToEndOfArray (a, b) {
+function destructivelyAddElementToBeginningOfArray (a, b) {
   a.unshift(b)
   return a
 }
